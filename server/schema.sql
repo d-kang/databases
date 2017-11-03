938eb18e9c38dc5069710d201723d543243a05f3
d-kang <david.kang714@gmail.com>
2017-11-03 10:21:41 -0700

    Add Sample Schema and Drop Dabass Script

--- server/schema.sql
+++ server/schema.sql
@@ -1,9 +1,18 @@
+DROP DATABASE chat;
 CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
+-- CREATE TABLE messages (
+--   /* Describe your table here.*/
+--
+-- );
+
+CREATE TABLE Studentssssss(
+  LastName varchar(30) NOT NULL,
+  FirstName varchar(30) NOT NULL,
+  StudId int NOT NULL,
+  PRIMARY KEY (StudId)
 );
 
 /* Create other tables and define schemas for them here! */
